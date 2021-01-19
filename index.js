32a9224f5473d83d4e2b8fbd537d1b1d2b4fa821
Faiyaz Rafeek <fayaznahwi@gmail.com>
2021-01-19 23:20:26 +0530

    partial deploy

---
+++ index.js
@@ -0,0 +1,44 @@
+
+const msg = document.getElementById("message");
+const submit = document.getElementById("btn-add");
+
+const database = firebase.database();
+const usersRef = database.ref('/');
+
+function writeData(){
+    if (msg.value != ""){
+        const autoId = usersRef.push().key
+        usersRef.push().set({
+        message: msg.value
+        });
+        Swal.fire(
+            'Added',
+            'Your message stored!',
+            'success'
+          )
+        msg.value = "";
+    }else{
+        Swal.fire(
+            'Empty Field',
+            'Enter your message in text filed',
+            'error'
+          )
+    }
+}
+
+function getData() {
+    usersRef.on('value', function(snapshot) {
+        let data =  Object.values(snapshot.val());
+        data.forEach(data => {
+            $('#view_area').append(`
+            <div class="card">
+                <div class="card-body">               
+                    <p>${data.message}</p>           
+                </div>
+            </div></br>
+            `)
+        });   
+    });
+}
+
+
